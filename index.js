9fe9adcdbd1a08efabb3f3005614968dbb5459ce
André Dalcin <andre@planosassessoria.com.br>
2017-03-16 20:58:42 -0300

    Projeto para transformar as keywords do PostgreSQL em maiusculas

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const fs = require('fs')
+const keys = require('./keys')
+
+let params = process.argv
+
+fs.readFile(params[2], (err, data) => {
+    if (err) throw err
+
+    let reg = new RegExp(keys.map(l => '\\b' + l.toLowerCase() + '\\b').toString().split(',').join('|'), 'g')
+
+    console.log(data.toString().replace(reg, key => key.toUpperCase()))
+})
+
