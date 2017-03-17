25d21ee6240a66ada2252eb81ebf867ed44e52ff
André Dalcin <andre@planosassessoria.com.br>
2017-03-17 09:48:17 -0300

    mudanças no algoritmo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let params = process.argv
 fs.readFile(params[2], (err, data) => {
     if (err) throw err
 
-    let reg = new RegExp(keys.map(l => '\\b' + l.toLowerCase() + '\\b').toString().split(',').join('|'), 'g')
+    let reg = new RegExp(keys.map(l => '\\b' + l.toLowerCase() + '\\b').join('|'), 'g')
 
     console.log(data.toString().replace(reg, key => key.toUpperCase()))
 })
